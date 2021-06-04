d0611e31aed87e69c36fe2b4c3e6c8aa4e3e29cd
Krzysztof Mulawa <mrkristoso@gmail.com>
2021-06-04 18:26:51 +0200

    Beta 1.51 CodeS WIP

--- 3DTransitions/qt3dtransitions.cpp
+++ 3DTransitions/qt3dtransitions.cpp
@@ -12,7 +12,7 @@ QT3DTransitions::QT3DTransitions(QWidget *parent)
     beginY = ui->mainFrame->y();
     img = new QImage(szer, wys, QImage::Format_RGB32);
     img->fill(Qt::black);
-    paintPrism();
+    on_resetButton_clicked();
 }
 
 QT3DTransitions::~QT3DTransitions()
@@ -44,6 +44,13 @@ void QT3DTransitions::drawLine(int x0, int y0, int x1, int y1)
     int x, y;
     a = (float)(y1 - y0) / (x1 - x0);
     b = y0 - a * x0;
+    if(x0 == x1)
+    {
+        for(int i = y0; i < y1; (y0 < y1) ? i++ : i--)
+        {
+            putPixel(x0, i);
+        }
+    }
     for(x = x0; x != x1; (x < x1) ? x++ : x--)
     {
         y = a * x + b;
@@ -74,40 +81,36 @@ void QT3DTransitions::paintPrism()
         vector[i].setX(vector[i].x() - szer / 2);
         vector[i].setY(vector[i].y() - wys / 2);
 
+        tmpY = ((vector[i].y() * cos(rotX)) - (vector[i].z() * sin(rotX)));
+        tmpZ = ((vector[i].y() * sin(rotX)) + (vector[i].z() * cos(rotX)));
+        vector[i].setY(tmpY);
+        vector[i].setZ(tmpZ);
 
-            tmpY = ((vector[i].y() * cos(rotX)) - (vector[i].z() * sin(rotX)));
-            tmpZ = ((vector[i].y() * sin(rotX)) + (vector[i].z() * cos(rotX)));
-            vector[i].setY(tmpY);
-            vector[i].setZ(tmpZ);
+        tmpX = ((vector[i].x() * cos(rotY)) - (vector[i].z() * sin(rotY)));
+        tmpZ = (((-1) * vector[i].x() * sin(rotY)) + (vector[i].z() * cos(rotY)));
+        vector[i].setX(tmpX);
+        vector[i].setZ(tmpZ);
 
-            tmpX = ((vector[i].x() * cos(rotY)) - (vector[i].z() * sin(rotY)));
-            tmpZ = (((-1) * vector[i].x() * sin(rotY)) + (vector[i].z() * cos(rotY)));
-            vector[i].setX(tmpX);
-            vector[i].setZ(tmpZ);
+        tmpY = ((vector[i].x() * cos(rotZ)) - (vector[i].y() * sin(rotZ)));
+        tmpZ = ((vector[i].x() * sin(rotZ)) + (vector[i].y() * cos(rotZ)));
+        vector[i].setX(tmpX);
+        vector[i].setY(tmpY);
 
-            tmpY = ((vector[i].x() * cos(rotZ)) - (vector[i].y() * sin(rotZ)));
-            tmpZ = ((vector[i].x() * sin(rotZ)) + (vector[i].y() * cos(rotZ)));
-            vector[i].setX(tmpX);
-            vector[i].setY(tmpY);
+        tmpY = vector[i].y() + (vector[i].z() * tiltY);
+        tmpZ = vector[i].z() + (vector[i].z() * tiltZ);
+        vector[i].setY(tmpY);
+        vector[i].setZ(tmpZ);
 
-        //tilt
+        tmpX = vector[i].x() + (vector[i].z() * tiltX);
+        tmpZ = vector[i].z() + (vector[i].z() * tiltZ);
+        vector[i].setX(tmpX);
+        vector[i].setZ(tmpZ);
 
-             tmpY = vector[i].y() + (vector[i].z() * tiltY);
-             tmpZ = vector[i].z() + (vector[i].z() * tiltZ);
-             vector[i].setY(tmpY);
-             vector[i].setZ(tmpZ);
+        tmpX = vector[i].x() + (vector[i].z() * tiltX);
+        tmpY = vector[i].y() + (vector[i].z() * tiltY);
+        vector[i].setX(tmpX);
+        vector[i].setY(tmpY);
 
-
-             tmpX = vector[i].x() + (vector[i].z() * tiltX);
-             tmpZ = vector[i].z() + (vector[i].z() * tiltZ);
-             vector[i].setX(tmpX);
-             vector[i].setZ(tmpZ);
-
-             tmpX = vector[i].x() + (vector[i].z() * tiltX);
-             tmpY = vector[i].y() + (vector[i].z() * tiltY);
-             vector[i].setX(tmpX);
-             vector[i].setY(tmpY);
-        //
         vector[i].setX((vector[i].x() * scaleX) + (szer / 2) + transX);
         vector[i].setY((vector[i].y() * scaleY) + (wys / 2) + transY);
         vector[i].setZ((vector[i].z() * scaleZ) + transZ);
@@ -115,19 +118,15 @@ void QT3DTransitions::paintPrism()
         vector[i].setX((vector[i].x() / ((vector[i].z() / 1000.0) + 1.0)));
         vector[i].setY((vector[i].y() / ((vector[i].z() / 1000.0) + 1.0)));
     }
-    drawLine(vector[2].x(), vector[2].y(), vector[3].x(), vector[3].y());
+    drawLine(vector[2].x(), vector[2].y(), vector[3].x(), vector[3].y()); //dół
     drawLine(vector[2].x(), vector[2].y(), vector[6].x(), vector[6].y());
     drawLine(vector[3].x(), vector[3].y(), vector[7].x(), vector[7].y());
     drawLine(vector[6].x(), vector[6].y(), vector[7].x(), vector[7].y());
-
-    //górna ściana
-    drawLine(vector[0].x(), vector[0].y(), vector[1].x(), vector[1].y());
+    drawLine(vector[0].x(), vector[0].y(), vector[1].x(), vector[1].y()); //góra
     drawLine(vector[0].x(), vector[0].y(), vector[4].x(), vector[4].y());
     drawLine(vector[4].x(), vector[4].y(), vector[5].x(), vector[5].y());
     drawLine(vector[5].x(), vector[5].y(), vector[1].x(), vector[1].y());
-
-    //ściany boczne
-    drawLine(vector[0].x(), vector[0].y(), vector[3].x(), vector[3].y());
+    drawLine(vector[0].x(), vector[0].y(), vector[3].x(), vector[3].y()); //ściany
     drawLine(vector[1].x(), vector[1].y(), vector[2].x(), vector[2].y());
     drawLine(vector[4].x(), vector[4].y(), vector[7].x(), vector[7].y());
     drawLine(vector[5].x(), vector[5].y(), vector[6].x(), vector[6].y());
